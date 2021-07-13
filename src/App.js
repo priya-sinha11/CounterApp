7cdb66a7288b621381b5935d87036927d50ac7bc
Priya Sinha <priyasinha028@gmail.com>
2021-07-13 19:56:25 +0530

    counter Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import './style.css'
+const App = ()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [score, setScore] = useState(0)
+    return(
+        <div>
+            <h1>Counter Application</h1>
+             <h3>Score {score}</h3>
+             <button onClick={()=>(score<25)? setScore(score+1) : "" }>Increment</button>
+             <button onClick={()=> (score>0)? setScore(score-1) : "" }>Decrement</button>
+             <button onClick={()=>{setScore(0)}}>Reset</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
